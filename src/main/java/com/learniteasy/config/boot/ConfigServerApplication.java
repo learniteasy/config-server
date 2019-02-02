8b731252d42adb2a7f4da923927232db95070e26
arahee001c <arahee001c@192.168.0.106>
2019-02-01 19:58:02 -0500

    Initial Commit

---
+++ src/main/java/com/learniteasy/config/boot/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.learniteasy.config.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author arahee001c
+ *
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
